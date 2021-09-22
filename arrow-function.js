af578cc4048372313f570f1ce81eee29e0f4e360
hanieh nojabaie <nojabaiehanieh@gmail.com>
2021-09-22 11:40:08 +0330

    arrow function
    practicing on different type of coding arrow function

---
+++ arrow-function.js
@@ -0,0 +1,27 @@
+function printmyName(name){
+  console.log(name)
+  
+}
+printmyName("hanieh")
+
+const printmyName2 =(name)=>{
+  console.log(name)
+  
+}
+printmyName2("ali")
+
+const printmyName3 = name =>{
+  console.log(name)
+  
+}
+printmyName3("sina")
+
+const myName4 = (name,age) => {
+    console.log(name,age)
+}
+
+myName4("maryam",28)
+
+const multiply = num =>  num*2
+console.log(multiply(2))
+
