516420f6f8ce45ac99fa013d825f860d2e60d639
hanieh nojabaie <nojabaiehanieh@gmail.com>
2021-09-22 11:05:14 +0330

    Add files via upload
    practicing on let and const variable

---
+++ let-const.js
@@ -0,0 +1,13 @@
+// let and var
+let myname="hanieh";
+console.log(myname);
+
+myname="fateme";
+console.log(myname);
+// const
+const yourname="ali"
+console.log(yourname)
+
+//yourname="hasan"
+//console.log(yourname)
+// this take an error
