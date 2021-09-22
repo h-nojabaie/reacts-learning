be3e8c0c80ef8a9aa97c3e39584a54bf2d6f3bd0
hanieh nojabaie <nojabaiehanieh@gmail.com>
2021-09-22 16:32:53 +0330

    spread
    practicing on spread in array and object

---
+++ spread.js
@@ -0,0 +1,13 @@
+// spread in array
+const number=[1,2,3]
+const newnum=[...number,4,5]
+console.log(newnum)
+
+// spread in object
+const person={name:"hanieh"}
+const newperson={
+  ...person,
+  age:12
+}
+
+console.log(newperson)
